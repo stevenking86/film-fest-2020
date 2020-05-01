ee78bd6000ff2614b4d3a1fa244be5f546d33e9e
Steven Gerhardt-King <stevenking86@gmail.com>
2020-05-01 14:06:22 -0400

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Quarantine Film Fest 2020`,
+    description: `Let's do this.`,
+    author: `stevengerhardtking`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,45 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
 import Image from "../components/image"
-import SEO from "../components/seo"
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  background-color: black;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+`;
+
+const ImageWrapper = styled.div`
+  margin: 0 auto;
+  max-width: 400px;
+`;
+
+const Headline = styled.h1`
+  text-align: center;
+  font-family: 'courier';
+  font-weight: 200;
+  padding-top: 20vh;
+  color: #C2FFFF;
+  font-size: 60px;
+`;
+
+const Para = styled.p`
+color: #C2FFFF;
+font-family: 'courier';
+`
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+  <Wrapper>
+    <Headline>Quarantine Film Fest 2020</Headline>
+    <ImageWrapper>
       <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+    </ImageWrapper>
+    <p>The best short films to get you through your stay-at-home order.</p>
+    <p>Submissions open soon.</p>
+  </Wrapper>
+);
 
 export default IndexPage
