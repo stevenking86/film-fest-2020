8f5475d75a0a9513cff342ec40cf315b8b61438d
Steven Gerhardt-King <stevenking86@gmail.com>
2020-06-26 12:18:22 -0400

    add blm charity

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -27,13 +27,14 @@ const Donate = () => (
   <>
     <Headline style={{marginTop: '16px', paddingTop: '0px'}}>Quarantine Film Fest 2020</Headline>
     <Para>
-      We're encouraging all who enjoy this festival to donate to an organization helping out with Covid-19 relief.  Please consider making a donation to one of the following organizations:
+      We're encouraging all who enjoy this festival to donate to an organization helping out with Covid-19 relief or the Black Lives Matter movement.  Please consider making a donation to one of the following organizations:
     </Para>
     <Para>
       <ul>
         <li><a target="_blank" rel="noopener noreferrer" href="https://www.nokidhungry.org/coronavirus">No Hungry Kids</a></li>
         <li><a target="_blank" rel="noopener noreferrer" href="https://www.unicefusa.org/?utm_campaign=20200323_Emergencies">Unicef</a></li>
         <li><a target="_blank" rel="noopener noreferrer" href="https://give.salvationarmyusa.org/give/276832">Salvation Army</a></li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/document/d/1vMBNCGLahTZa50vQOEPFI_-7BQzyOPInwLj_jNMzR2c/mobilebasic?urp=gmail_link">Donate to Support Black Lives</a></li>
       </ul>
     </Para>
 
